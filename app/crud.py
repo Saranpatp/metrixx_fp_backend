a68f6b60b4991e1f3043d82248ba698f561f6257
Saranpat Prasertthum <Saranpatp@gmail.com>
2024-05-15 15:44:35 -0500

    + add example function

--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,19 @@
 from sqlalchemy.orm import Session
 from . import models
 
+from datetime import datetime
+
 def get_market_data(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.MarketData).offset(skip).limit(limit).all()
 
 def get_market_data_by_symbol(db: Session, symbol: str):
     return db.query(models.MarketData).filter(models.MarketData.symbol == symbol).all()
+
+# TODO create a function to get prev min data with parameter (symbol and start_time and end_time)
+
+def get_market_data_by_symbol_time(db: Session, symbol: str, start_time: datetime, end_time: datetime):
+    return db.query(models.MarketData).filter(
+        models.MarketData.symbol == symbol,
+        models.MarketData.datetime >= start_time,
+        models.MarketData.datetime <= end_time
+    ).all()
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time
 from sqlalchemy.orm import Session
 from typing import List
+from datetime import datetime, timedelta
 
 from app import models, crud, schemas
 from app.database import SessionLocal, engine
+import pandas as pd
 
 # Initialize the database
 models.Base.metadata.create_all(bind=engine)
@@ -17,14 +19,22 @@ def get_db():
 
 def fetch_and_print_latest_data():
     with next(get_db()) as db:
-        market_data = crud.get_market_data(db, skip=0, limit=10)
-        for data in market_data:
-            print(data.id)
+        curr_time = datetime.now()
+        market_data = crud.get_market_data_by_symbol_time(db, symbol='MES 06-24', start_time= curr_time - timedelta(minutes=1), end_time=curr_time)
+        data = [result.__dict__ for result in market_data]
+    
+    # Remove the SQLAlchemy instance state from each dictionary
+    for item in data:
+        item.pop('_sa_instance_state', None)
+    
+    # Convert to a DataFrame
+    df = pd.DataFrame(data)
+    print(df.head())
 
 def main():
     while True:
         fetch_and_print_latest_data()
-        time.sleep(15)
+        time.sleep(60)
 
 if __name__ == "__main__":
     main()
