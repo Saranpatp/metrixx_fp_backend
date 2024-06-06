d3d160a0595a649e9942e306b4710f731574f985
wanchai2 <wanchai2@illinois.edu>
2024-06-06 13:41:51 -0500

    add comment to liquidity_index.py and main.py

--- liquidity_index.py
+++ liquidity_index.py
@@ -19,6 +19,7 @@ def get_db():
     finally:
         db.close()
 
+# Fetch data function can specify start_time and end_time
 def fetch_and_print_data(start_time = datetime.min, end_time = datetime.now(), symbol = 'MES 06-24'):
     with next(get_db()) as db:
         curr_time = datetime.now()
@@ -33,9 +34,11 @@ def fetch_and_print_data(start_time = datetime.min, end_time = datetime.now(), s
     df = pd.DataFrame(data)
     return df
 
-def foot_print_transformation_time_frame(df, time_frame_min=1):
+# function to transform data to footprint data. This function allow to select time length for each footprint candle
+def footprint_transformation_time_frame(df, time_frame_min=1):
     df = df[['symbol','datetime','last_price','trades', 'total_volume', 'total_buying_volume','total_selling_volume']]
     df.columns = ['Symbol', 'Time', 'Price', 'Trades', 'TotalVolume', 'Bid', 'Ask']
+    # This is to round down to time to the beginning of candle's time frame
     df['Time'] = df['Time'].apply(lambda t: datetime(t.year, t.month, t.day, t.hour,time_frame_min*(t.minute // time_frame_min)))
 
     #Create footprint chart data
@@ -48,19 +51,24 @@ def foot_print_transformation_time_frame(df, time_frame_min=1):
     footprint_data = footprint_data.sort_values(by=['Time', 'Price'])
     return(footprint_data)
 
+# bid_ask_spread function returns original bid-ask spread and weighted price spread
 def bid_ask_spread(df, time_frame_min = 1):
-    footprint = foot_print_transformation_time_frame(df, time_frame_min=time_frame_min)
+    # transform second-by-second data to footprint data
+    footprint = footprint_transformation_time_frame(df, time_frame_min=time_frame_min)
+    
+    # Finding  Weighted Price manually
     footprint['PriceXBid'] = footprint['Price'] * footprint['Bid']
     footprint['PriceXAsk'] = footprint['Price'] * footprint['Ask']
-    
-    # Finding Price
     weighted_average = footprint.groupby('Time').agg(PriceXBid=('PriceXBid', sum), PriceXAsk=('PriceXAsk', sum), Bid=('Bid',sum), Ask=('Ask',sum))
     weighted_average['Weighted_Bid_Price'] = np.where(weighted_average['Bid'] != 0, weighted_average['PriceXBid']/weighted_average['Bid'], np.nan)
     weighted_average['Weighted_Ask_Price'] = np.where(weighted_average['Ask'] != 0, weighted_average['PriceXAsk']/weighted_average['Ask'], np.nan)
+    
+    # finding max_bid_price, min_ask_price for each candle (time)
+    # Spread = max_bid_price - min_ask_price
     max_BidPrice = footprint[footprint['Bid'] != 0].groupby('Time')['Price'].max().reset_index(name='Max_Bid')
     min_AskPrice = footprint[footprint['Ask'] != 0].groupby('Time')['Price'].min().reset_index(name='Min_Ask')
 
-    # Merge the results on 'Time'
+    # Merge the results based on 'Time' and compute spread
     spread = pd.merge(weighted_average, min_AskPrice, on='Time', how='left')
     spread = pd.merge(spread, max_BidPrice, on='Time')
     spread['Bid_Ask_Spread'] = spread['Min_Ask']-spread['Max_Bid']
@@ -68,17 +76,20 @@ def bid_ask_spread(df, time_frame_min = 1):
     spread = spread[['Time','Bid_Ask_Spread','Weighted_Bid_Ask_Spread']]
     return spread
 
-
+# turn_over function takes second-by-second data as an input
 def turn_over_ratio(df, time_frame_min = 1):
+    # This is to round down to time to the beginning of candle's time frame
     df['Time'] = df['datetime'].apply(lambda t: datetime(t.year, t.month, t.day, t.hour,time_frame_min*(t.minute // time_frame_min)))
+    # sum traded and total trading volume
     turnover_df = df.groupby(['Time']).agg(
         Trades=('trades', 'sum'),  # Sum the 'Bid' column
         TotalVolume=('total_volume', 'sum')   # Sum the 'Ask' column
     ).reset_index() 
+    # turnover ratio = traded volume/total trading volume (order volume)
     turnover_df['Turnover_Ratio'] = turnover_df['Trades']/turnover_df['TotalVolume']
     turnover_df = turnover_df[['Time','Turnover_Ratio']]
     return turnover_df
-
+# This function is to get all indices, including bid-ask spread, weighted bid-ask spread, and turnover ratio, at once
 def get_liquidity_index(start_time = datetime.min, end_time = datetime.now(), symbol = 'MES 06-24', time_frame_min = 1):
     df = fetch_and_print_data(start_time, end_time, symbol= symbol)
     BidAsk = bid_ask_spread(df, time_frame_min=time_frame_min)
@@ -86,7 +97,7 @@ def get_liquidity_index(start_time = datetime.min, end_time = datetime.now(), sy
     result = pd.merge(BidAsk, turnover, on='Time', how='left')
     return result
 
-
+# Get indices and print (can adjust time frame of indices)
 result = get_liquidity_index(time_frame_min=30)
 print(result.head(20))
 
--- main.py
+++ main.py
@@ -11,8 +11,10 @@ import csv
 
 # Initialize the database
 models.Base.metadata.create_all(bind=engine)
+# Add more tickers to the infinite loop here
 tickers = ['MES 06-24', 'ES 06-24']
 
+# get database connection
 def get_db():
     db = SessionLocal()
     try:
@@ -20,6 +22,7 @@ def get_db():
     finally:
         db.close()
 
+# fetch latest data in the past 60 seconds (from now-60s to now) 
 def fetch_and_print_latest_data():
     with next(get_db()) as db:
         curr_time = datetime.now()
@@ -35,6 +38,7 @@ def fetch_and_print_latest_data():
     print(df.head())
     return df
 
+# fetch data of the full previous minute data (eg. if now is 12:15:30, return data from to 12:14:00 - 12:14:59 )
 def fetch_and_print_last_min_data(symbol = 'MES 06-24'):
     with next(get_db()) as db:
         curr_time = datetime.now().replace(second=0) - timedelta(seconds=1)
@@ -50,12 +54,14 @@ def fetch_and_print_last_min_data(symbol = 'MES 06-24'):
     print(df.head())
     return df
 
+# Function to mimic footprint chart data
 def foot_print_transformation(df):
+    # select and format data
     df = df[['symbol','datetime','last_price','trades', 'total_volume', 'total_buying_volume','total_selling_volume']]
     df.columns = ['Symbol', 'Time', 'Price', 'Trades', 'TotalVolume', 'Bid', 'Ask']
     df['Time'] = pd.to_datetime(df['Time']).dt.floor('Min')
 
-    #Create footprint chart data
+    #Create footprint chart data (compute bid and ask at prices in a time period)
     footprint_data = df.groupby(['Time', 'Price']).agg(
         Bid=('Bid', 'sum'),
         Ask=('Ask', 'sum'),
@@ -66,15 +72,22 @@ def foot_print_transformation(df):
     footprint_data = footprint_data.sort_values(by=['Time', 'Price'])
     return(footprint_data)
 
+# Function to detect volume cluster
 def volume_cluster(footprint, tick_size = 0.25, cluster_param = 5):
     result = pd.DataFrame()
+    #sort volume descending to see what prices have highest/lowest volume
     price_by_time = footprint.sort_values(by=['Volume'],ascending=False)
+    #get time (there is only one time in a footprint data)
     time = footprint.loc[0,'Time']
+    #count unique price at a time
     price_step = price_by_time.shape[0]
+    #filter the most volume price up to number of cluster parameter
     price_by_time = price_by_time[0:cluster_param]
+    #check if there is price step more than the cluster parameter. If not, there is no way for volume cluster
     if price_step >= cluster_param:
         max = price_by_time['Price'].max()
         min = price_by_time['Price'].min()
+        #there is volume cluster if and only if number of tick between max and min; (max-min)/tick_price = cluster_param - 1
         if (max - min)/tick_size == cluster_param-1:
             result = pd.concat([result, pd.DataFrame([['Volume Cluster', time, max]],columns=['Event', 'Time', 'Price'])])
     if result.empty:
@@ -82,9 +95,10 @@ def volume_cluster(footprint, tick_size = 0.25, cluster_param = 5):
     
     return result
 
+# Function to detect selling and buying imbalance
 def imbalance(footprint, tick_size = 0.25, stacked_param = 3, imbalance_param = 3):
     time = footprint.loc[0,'Time']
-    #compute imbalance and preparing shifted data
+    #compute imbalance and preparing shifted data (we compute differnce between bid at price x and ask at price x+1_tick)
     testdf = footprint.sort_values(by=['Price'],ascending=False)
     temp = pd.DataFrame(np.arange (testdf['Price'].min(),testdf['Price'].max()+tick_size,tick_size)).rename(columns={0: "Price"})
     temp = temp.sort_values(by=['Price'],ascending=False)
@@ -94,9 +108,10 @@ def imbalance(footprint, tick_size = 0.25, stacked_param = 3, imbalance_param = 
     temp = temp.rename(columns={'Price_x': "Price"})
     temp = temp[['Price','Bid','Ask']]
     temp['imbalance'] = abs(temp['Bid'] - temp['Ask']) / np.minimum(temp['Bid'], temp['Ask'])
-    # to litigate divided by zero
+    # to handle n/a and undefined error
     temp.fillna(0, inplace=True)
     temp = temp.replace([np.inf, -np.inf], 0)
+
     # create a list of our conditions
     conditions = [
        (temp['imbalance'] >= imbalance_param) & (temp['Bid'] > temp['Ask']),
@@ -107,19 +122,22 @@ def imbalance(footprint, tick_size = 0.25, stacked_param = 3, imbalance_param = 
     # create a new column and use np.select to assign values to it using our lists as arguments
     temp['Imbalance_Flag'] = np.select(conditions, values)
     data = temp[['Price', 'Imbalance_Flag']]
-    #start to find stack imbalance
+    #start to find stack imbalance and set counting parameter
     result = pd.DataFrame()
     count = 0
     stack_price = 0
     current_flag = 'n'
     for price, flag in data.itertuples(index=False):
+        # in case previous and current flag are the same, count + 1
         if flag in ('Selling Imbalance', 'Buying Imbalance') and flag == current_flag:
             count +=1
             current_flag = flag
+        # if not, reset the count
         else:
             count = 1
             current_flag = flag
             stack_price = price
+        # Signal the event if count = imbalance parameter
         if count == stacked_param:
             result = pd.concat([result, pd.DataFrame([[flag, time, stack_price]],columns=['Event', 'Time', 'Price'])])
     if result.empty:
@@ -127,73 +145,95 @@ def imbalance(footprint, tick_size = 0.25, stacked_param = 3, imbalance_param = 
     
     return result
 
+# Function to detect multiple high volume node
 def multiple_high_volume_node(footprint, n_node = 2, last_price = 0, multiple_count = 1):
     time = footprint.loc[0,'Time']
     result = pd.DataFrame()
+    #get the highest volume price
     price_by_time = footprint.sort_values(by=['Volume'],ascending=False)
     price = price_by_time['Price'].iloc[0]
+    # check if price = last price. If so, multiple_count + 1
     if price == last_price:
         multiple_count += 1
+        # when multiple_count reaches predefinec n_node, signal an event.
         if multiple_count == n_node:
             result = pd.concat([result, pd.DataFrame([['Multiple High Volume Node', time, price]],columns=['Event', 'Time', 'Price'])])
+    # if price != last price, reset multiple_count
     else:
         multiple_count = 1    
     
     if result.empty:
         result = pd.concat([result, pd.DataFrame([['NaN', time, 0]],columns=['Event', 'Time', 'Price'])])
-    
+    #return price and multiple_count to be input for the next loop
     return price, multiple_count, result
 
+# Function to detect zero print
 def zero_print(footprint):
     result = pd.DataFrame()
     time = footprint.loc[0,'Time']
+    #sort value by price to mimic a foot print candles
     df = footprint.sort_values(by=['Price'],ascending=False)
+    # there is Bid Zero Print if Bid = 0 at the lowest price
     if df['Bid'].iloc[-1] == 0:
         result = pd.concat([result, pd.DataFrame([['Bid Zero Print', time, df['Price'].iloc[-1]]],columns=['Event', 'Time', 'Price'])])
+    # there is Ask Zero Print if Ask = 0 at the highest price
     if df['Ask'].iloc[0] == 0:
         result = pd.concat([result, pd.DataFrame([['Ask Zero Print', time, df['Price'].iloc[0]]],columns=['Event', 'Time', 'Price'])])
+    # return price = 0 if there is no Zero Print
     if result.empty:
         result = pd.concat([result, pd.DataFrame([['NaN', time, 0]],columns=['Event', 'Time', 'Price'])])
     return result
 
+# Function to detect failed auction
 def failed_auction(footprint):
     result = pd.DataFrame()
     time = footprint.loc[0,'Time']
+    # sort value by price to mimic a foot print candles
     df = footprint.sort_values(by=['Price'],ascending=False)
+    # there is failed auction - Bid high if bid is not zero at the highest price
     if df['Bid'].iloc[0] != 0:
         result = pd.concat([result, pd.DataFrame([['Failed Auction - Bid High', time, df['Price'].iloc[0]]],columns=['Event', 'Time', 'Price'])])
+    # there is failed auction - Ask low if Ask is not zero at the lowest price
     if df['Ask'].iloc[-1] != 0:
-        result = pd.concat([result, pd.DataFrame([['Fail Auction - Ask Low', time, df['Price'].iloc[-1]]],columns=['Event', 'Time', 'Price'])])
+        result = pd.concat([result, pd.DataFrame([['Failed Auction - Ask Low', time, df['Price'].iloc[-1]]],columns=['Event', 'Time', 'Price'])])
+    # return price = 0 if there is no failed auction
     if result.empty:
         result = pd.concat([result, pd.DataFrame([['NaN', time, 0]],columns=['Event', 'Time', 'Price'])])
     return result
 
+# this is the main fuction that call all functions to query data, detect foot print events, and send notifications 
 def main():
+    # create a dictionary for last_price and multiple_count to be used as input for multiple_high_volume detection
     last_price_dict = {ticker: 0 for ticker in tickers}
     multiple_count_dict = {ticker: 1 for ticker in tickers}
+    # start time for log file
     start_time = datetime.now().strftime("%Y-%m-%d_%H-%M")
+    # infinite loop to query data and event detection
     while True:
+        # Loop for all tickers at the top
         for ticker in tickers:
-            #fetch_and_print_latest_data()
+            # Fetch last minute data, if error skip to the next loop
             try:
                 df = fetch_and_print_last_min_data(ticker)
             except Exception as e:
                 print(e)
             if df.empty:
                 continue
+            # transform to foottprint data
             footprint = foot_print_transformation(df)
+            # Detect volume cluster, if it is found sent notification and save to log file
             cluster_result = volume_cluster(footprint)
             cluster_result.insert(loc=0, column='Symbol', value=ticker)
             if cluster_result['Price'][0] != 0:
                 notification.sent_msg(f"{cluster_result['Symbol'][0]}: {cluster_result['Event'][0]} at {cluster_result['Price'][0]} USD at {cluster_result['Time'].dt.strftime('%Y-%m-%d %H:%M')[0]}")
                 cluster_result.to_csv(f'log_{start_time}.csv', sep='\t', header=None, mode='a')
-            
+            # Detect imbalance, if it is found sent notification and save to log file
             imbalance_result = imbalance(footprint)
             imbalance_result.insert(loc=0, column='Symbol', value=ticker)
             if imbalance_result['Price'][0] != 0:
                 notification.sent_msg(f"{imbalance_result['Symbol'][0]}: {imbalance_result['Event'][0]} at {imbalance_result['Price'][0]} USD at {imbalance_result['Time'].dt.strftime('%Y-%m-%d %H:%M')[0]}")
                 imbalance_result.to_csv(f'log_{start_time}.csv', sep='\t', header=None, mode='a')
-
+            # Detect multiple high volume node, if it is found sent notification and save to log file
             price, multiple_count_new, multiple_result = multiple_high_volume_node(footprint, n_node=2, last_price=last_price_dict[ticker], multiple_count=multiple_count_dict[ticker])
             multiple_result.insert(loc=0, column='Symbol', value=ticker)
             if multiple_result['Price'][0] != 0:
@@ -201,19 +241,20 @@ def main():
                 multiple_result.to_csv(f'log_{start_time}.csv', sep='\t', header=None, mode='a')
             last_price_dict[ticker] = price
             multiple_count_dict[ticker] = multiple_count_new
-            
+            # Detect zero print, if it is found sent notification and save to log file
             zero_print_result = zero_print(footprint)
             zero_print_result.insert(loc=0, column='Symbol', value=ticker)
             if zero_print_result['Price'].iloc[0] != 0:
                 notification.sent_msg(f"{zero_print_result['Symbol'].iloc[0]}: {zero_print_result['Event'].iloc[0]} at {zero_print_result['Price'].iloc[0]} USD at {zero_print_result['Time'].dt.strftime('%Y-%m-%d %H:%M').iloc[0]}")
                 zero_print_result.to_csv(f'log_{start_time}.csv', sep='\t', header=None, mode='a')
-
+            # Detect failed auction, if it is found sent notification and save to log file
             failed_auction_result = failed_auction(footprint)
             failed_auction_result.insert(loc=0, column='Symbol', value=ticker)
             if failed_auction_result['Price'].iloc[0] != 0:
                 notification.sent_msg(f"{failed_auction_result['Symbol'].iloc[0]}: {failed_auction_result['Event'].iloc[0]} at {failed_auction_result['Price'].iloc[0]} USD at {failed_auction_result['Time'].dt.strftime('%Y-%m-%d %H:%M').iloc[0]}")
                 failed_auction_result.to_csv(f'log_{start_time}.csv', sep='\t', header=None, mode='a')
-
+        
+        # once loop all the tickers in a minute, stop for 60 secs to do another loop. This is to make sure that we get data from different time period
         time.sleep(60)
 
 if __name__ == "__main__":
